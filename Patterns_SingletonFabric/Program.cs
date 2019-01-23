8604b7e33190954285a526e217f7be31dab82f94
Старцев Даниил Сергеевич <dsstartsev@edu.hse.ru>
2019-01-23 22:02:34 +0500

    Реализован паттерн "Одиночка". Показана проблема паттерна

--- Patterns_SingletonFabric/Program.cs
+++ Patterns_SingletonFabric/Program.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Patterns_SingletonFabric
 {
     class Program
     {
+
+        public static void InstanceExample()
+        {
+            Console.WriteLine("Первый запуск");
+            InstancePreview();
+            Singleton.Clear();
+
+
+            Console.WriteLine("\nВторой запуск");
+            InstancePreview(3, 0);
+            Singleton.Clear();
+        }
+
+        public static void InstancePreview(int n = 3, int sleepTime = 1000)
+        {
+            Singleton instance = Singleton.GetInstance();
+            Console.WriteLine(instance.Message);
+            for (int i = 0; i < n; i++)
+            {
+                
+                Thread thread = new Thread(InstanceJob);
+                thread.Start();
+                //Проблема параллельности - необходим контроль доступа к ресурсам
+                Thread.Sleep(sleepTime);
+            }
+
+            Console.WriteLine(instance.UserCount);
+        }
+        
+        public static void InstanceJob()
+        {
+            Singleton instance = Singleton.GetInstance();
+            Console.WriteLine(instance.Message);
+        }
+
         static void Main(string[] args)
         {
+            InstanceExample();
         }
     }
 }
--- Patterns_SingletonFabric/Singleton.cs
+++ Patterns_SingletonFabric/Singleton.cs
@@ -10,14 +10,38 @@ namespace Patterns_SingletonFabric
     {
         private static Singleton instance;
 
-        private Singleton()
-        { }
+        public int UserCount;
+        public string Message;
 
-        public static Singleton getInstance()
+        private string data;
+        public string GlobalData
         {
+            get { return data; }
+            set { data = value; }
+        }
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+
             if (instance == null)
+            {
                 instance = new Singleton();
+                instance.UserCount = 0;
+            }
+            instance.UserCount++;
+            instance.Message = "There are " + instance.UserCount + " users total";
+             
             return instance;
+        }
+        public static void Clear()
+        {
+            Singleton instance = GetInstance();
+            instance.UserCount = 0;
+            instance.Message = "Cleared";
         }
     }
 
